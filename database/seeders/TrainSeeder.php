34edb5d922b46f49eb5eb82f8b554886f13d79b9
Adriatik Berdufi <adriatik.berdufi@gmail.com>
2024-03-22 07:57:57 +0100

    add: first seeder

--- database/seeders/TrainSeeder.php
+++ database/seeders/TrainSeeder.php
@@ -15,5 +15,17 @@ class TrainSeeder extends Seeder
     public function run()
     {
         $train = new Train;
+
+        $train->id= 1;
+        $train->company= "trenItalia";
+        $train->departure_name= 'torino';
+        $train->arrival_name= 'padova';
+        $train->departure_time= '2024-03-22 09:01:01';
+        $train->arrival_time= '2024-03-22 14:05:01';
+        $train->train_code='AZ1923';
+        $train->carroza_number= '3';
+        $train->on_time= '1';
+        $train->cancelled='0';
+        $train->save();
     }
 }
